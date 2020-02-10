de93f7ef91e6a26c95f6f1f6e7ab989024552df6
Onkar Khaladkar <khaladkaronkar@gmail.com>
2020-02-10 11:42:40 +0530

    App Color

--- app/src/main/java/com/example/vradonation/loadingImage/LoadingImage.java
+++ app/src/main/java/com/example/vradonation/loadingImage/LoadingImage.java
@@ -16,6 +16,7 @@ public class LoadingImage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_loading_image, container, false);
+
         return view;
     }
 }
