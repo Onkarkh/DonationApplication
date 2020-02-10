2c5c1497f87f7cc66cdec1114d4f869a05175465
Onkar Khaladkar <khaladkaronkar@gmail.com>
2020-02-10 11:33:38 +0530

    Loading Image

--- app/src/main/java/com/example/vradonation/MainActivity.java
+++ app/src/main/java/com/example/vradonation/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.vradonation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.FrameLayout;
+
+import com.example.vradonation.loadingImage.LoadingImage;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.mainFrame);
+
+        loadingImage();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }, 300);
+    }
+
+    private void loadingImage() {
+        Fragment loadingImage = new LoadingImage();
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, loadingImage).commit();
     }
 }
